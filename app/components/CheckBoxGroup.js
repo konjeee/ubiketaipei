61e61c481eca81f8c2b9818e8cecf905a035c6e9
劻 <LedoBodzaW0913@gmail.com>
2023-10-24 07:02:50 +0800

    update CheckBoxGroup.js

--- app/components/CheckBoxGroup.js
+++ app/components/CheckBoxGroup.js
@@ -2,9 +2,17 @@ import React, { useEffect, useState } from "react";
 import CheckBox from "./CheckBox";
 import styles from "./CheckBoxGroup.module.css";
 
-const CheckBoxGroup = ({ options, onSelect }) => {
+const CheckBoxGroup = ({ options, onSelect, selectedCity }) => {
   const [checkedItems, setCheckedItems] = useState({});
   const [checkAll, setCheckAll] = useState(false);
+  const [filteredOptions, setFilteredOptions] = useState([]);
+
+  useEffect(() => {
+    const cityFilteredOptions = options.filter(
+      (option) => option.city == selectedCity
+    );
+    setFilteredOptions(cityFilteredOptions);
+  }, [selectedCity]);
 
   const handleCheckBoxChange = (district) => {
     const newCheckedItems = { ...checkedItems };
@@ -17,7 +25,7 @@ const CheckBoxGroup = ({ options, onSelect }) => {
     setCheckAll(newCheckAll);
 
     if (newCheckAll) {
-      const allDistricts = options.map((option) => option.district);
+      const allDistricts = filteredOptions.map((option) => option.district);
       const allChecked = {};
       allDistricts.forEach((district) => {
         allChecked[district] = true;
@@ -49,10 +57,10 @@ const CheckBoxGroup = ({ options, onSelect }) => {
           />
           全部勾選
         </label>
-        {options.map((option) => (
+        {filteredOptions.map((option) => (
           <CheckBox
+            key={option.id}
             district={option.district}
-            key={option.district}
             checked={checkedItems[option.district] || false}
             onChange={() => handleCheckBoxChange(option.district)}
           />
