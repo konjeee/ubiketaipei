bf997862b601cb738476632e540b40ec4aaafd4f
劻 <LedoBodzaW0913@gmail.com>
2023-10-25 01:06:31 +0800

    update CheckBox.js
    add id

--- app/components/CheckBox.js
+++ app/components/CheckBox.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "./CheckBoxGroup.module.css";
 
-const CheckBox = ({ district, checked, onChange }) => {
+const CheckBox = ({ id, district, checked, onChange }) => {
   return (
     <div className={styles.checkBoxForm}>
       <label className={styles.district}>
         <input
           type="checkbox"
+          id={id}
           checked={checked}
           onChange={onChange}
           className={styles.checkboxInput}
