1c220c379290124429f34fd4010ad36bed778974
劻 <LedoBodzaW0913@gmail.com>
2023-10-24 17:43:57 +0800

    update CheckBoxGroup.js

--- app/components/CheckBoxGroup.js
+++ app/components/CheckBoxGroup.js
@@ -3,9 +3,9 @@ import CheckBox from "./CheckBox";
 import styles from "./CheckBoxGroup.module.css";
 
 const CheckBoxGroup = ({ options, onSelect, selectedCity }) => {
-  const [checkedItems, setCheckedItems] = useState({});
-  const [checkAll, setCheckAll] = useState(false);
   const [filteredOptions, setFilteredOptions] = useState([]);
+  const [allChecked, setAllChecked] = useState(true);
+  const [checkedItems, setCheckedItems] = useState([]);
 
   useEffect(() => {
     const cityFilteredOptions = options.filter(
@@ -13,28 +13,6 @@ const CheckBoxGroup = ({ options, onSelect, selectedCity }) => {
     );
     setFilteredOptions(cityFilteredOptions);
   }, [selectedCity]);
-
-  const handleCheckBoxChange = (district) => {
-    const newCheckedItems = { ...checkedItems };
-    newCheckedItems[district] = !newCheckedItems[district];
-    setCheckedItems(newCheckedItems);
-  };
-
-  const handleCheckAllChange = () => {
-    const newCheckAll = !checkAll;
-    setCheckAll(newCheckAll);
-
-    if (newCheckAll) {
-      const allDistricts = filteredOptions.map((option) => option.district);
-      const allChecked = {};
-      allDistricts.forEach((district) => {
-        allChecked[district] = true;
-      });
-      setCheckedItems(allChecked);
-    } else {
-      setCheckedItems({});
-    }
-  };
 
   useEffect(() => {
     const selectedDistricts = Object.keys(checkedItems).filter(
@@ -44,6 +22,49 @@ const CheckBoxGroup = ({ options, onSelect, selectedCity }) => {
     onSelect(selectedDistricts);
   }, [checkedItems]);
 
+  const handleCheckBoxChange = (option) => {
+    const newFilteredOptions = filteredOptions.map((item) => {
+      if (item.id === option.id) {
+        return {
+          ...item,
+          checked: !item.checked,
+        };
+      }
+      return item;
+    });
+
+    setFilteredOptions(newFilteredOptions);
+
+    const newCheckedItems = {};
+    newFilteredOptions.forEach((item) => {
+      newCheckedItems[item.district] = item.checked;
+    });
+
+    setCheckedItems(newCheckedItems);
+
+    const areAllChecked = newFilteredOptions.every((item) => item.checked);
+    setAllChecked(areAllChecked);
+  };
+
+  const handleCheckAllChange = () => {
+    const newChecked = !allChecked;
+    setAllChecked(newChecked);
+
+    const updatedOptions = filteredOptions.map((option) => ({
+      ...option,
+      checked: newChecked,
+    }));
+
+    setFilteredOptions(updatedOptions);
+
+    const newCheckedItems = {};
+    updatedOptions.forEach((option) => {
+      newCheckedItems[option.district] = newChecked;
+    });
+
+    setCheckedItems(newCheckedItems);
+  };
+
   return (
     <>
       <div className={styles.checkBoxFormGroup}>
@@ -51,7 +72,7 @@ const CheckBoxGroup = ({ options, onSelect, selectedCity }) => {
           <input
             type="checkbox"
             id="checkAll"
-            checked={checkAll}
+            checked={allChecked}
             onChange={handleCheckAllChange}
             className={styles.checkBox}
           />
@@ -61,8 +82,8 @@ const CheckBoxGroup = ({ options, onSelect, selectedCity }) => {
           <CheckBox
             key={option.id}
             district={option.district}
-            checked={checkedItems[option.district] || false}
-            onChange={() => handleCheckBoxChange(option.district)}
+            checked={option.checked}
+            onChange={() => handleCheckBoxChange(option)}
           />
         ))}
       </div>
