fc02f2bad4b0c2ee52a37baba07bab31a6d47562
劻 <LedoBodzaW0913@gmail.com>
2023-10-25 23:08:55 +0800

    update Select.js
    add close dropdown useRef

--- app/components/Select.js
+++ app/components/Select.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Select.module.css";
 
 const Select = ({ options, selectedValue, onSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isDefault, setIsDefault] = useState(true);
+  const selectRef = useRef(null);
 
   const handleSelect = (value) => {
     onSelect(value);
@@ -11,8 +12,22 @@ const Select = ({ options, selectedValue, onSelect }) => {
     setIsDefault(false);
   };
 
+  const handleGlobalClick = (event) => {
+    if (selectRef.current && !selectRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleGlobalClick);
+
+    return () => {
+      document.removeEventListener("click", handleGlobalClick);
+    };
+  }, []);
+
   return (
-    <div className={styles.selectWrapper}>
+    <div ref={selectRef} className={styles.selectWrapper}>
       <div
         className={styles.selectHeader}
         onClick={() => setIsOpen(!isOpen)}
